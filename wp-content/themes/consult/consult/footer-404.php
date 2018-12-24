607bd592bd95317c563f021ec0ac4f8e8b1aec7b
Bhumi Sukhadiya <bhumika.sukhadiya.9@gmail.com>
2018-12-24 23:04:23 +0530

    change wordpress version

--- wp-content/themes/consult/consult/footer-404.php
+++
@@ -1,4 +0,0 @@
- <?php $consult_redux_demo = get_option('consult_redux_demo');?> 
- <?php wp_footer(); ?>
-    </body>
-</html>
--- wp-content/themes/consult/consult/sidebar-other.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * The Sidebar containing the main widget area
- */
-?>
-<?php if ( is_active_sidebar( 'sidebar-other' ) ) : ?>
-	<?php dynamic_sidebar( 'sidebar-other' ); ?>
-<?php endif; ?>
